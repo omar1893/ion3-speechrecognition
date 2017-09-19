861c6e7a728811db603effb5353adf6d3bc62684
Omar López <olopez@momentumlab.co>
2017-09-19 10:43:12 -0430

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  bgcolor: string = 'white';
 
+  constructor(public navCtrl: NavController, private speech: SpeechRecognition) {
+
+  }
+
+  ngOnInit() {
+    this.speech.hasPermission()
+      .then((hasPermission: boolean) => {
+
+        if (!hasPermission) {
+          this.speech.requestPermission()
+            .then(
+            () => console.log('Granted'),
+            () => console.log('Denied')
+            )
+        }
+
+      })
+  }
+
+  start(){
+    this.speech.startListening()
+    .subscribe(
+      (matches:Array<string>) => {
+        this.bgcolor = matches[0];
+      }
+    )
   }
 
 }
